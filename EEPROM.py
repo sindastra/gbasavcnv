9c249847618bd6f6826ab8d05e681ba84c2ffc77
Sindastra <sindastra+github@gmail.com>
2018-02-16 16:44:21 +0100

    Initial commit of EEPROM.py (EEPROM converter).

---
+++ EEPROM.py
@@ -0,0 +1,14 @@
+#!/bin/python
+#
+# EEPROM .sav dump to emulator/flashcard .sav converter.
+# Copyright (C) 2018 Sindastra <https://github.com/sindastra/gbasavcnv>.
+# All rights reserved.
+#
+# The above copyright notice and this notice shall be included in all
+# copies or substantial portions of the Software. Do not remove this.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# included LICENSE file.
+
